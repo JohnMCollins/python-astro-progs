544c485cc998c59b7bf22aa3fe9fa782e374aae8
John Collins <j.m.collins@herts.ac.uk>
2014-03-26 15:59:08 +0000

    end Wed

--- Numpy/continuum/continuum.py
+++ Numpy/continuum/continuum.py
@@ -5,16 +5,28 @@
 import argparse
 import os.path
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
 
 import specdatactrl
 import datarange
 import xmlutil
 import meanval
 
+class intresult(object):
+    """Record integration results"""
+
+    def __init__(self, da):
+        self.dataarray = da
+        self.bluecont = 0.0
+        self.redcont = 0.0
+        self.totcont = 0.0
+
 parsearg = argparse.ArgumentParser(description='Calculate continuum')
 parsearg.add_argument('--rangefile', type=str, help='Range file')
 parsearg.add_argument('--specfile', type=str, help='Spectrum data controlfile')
 parsearg.add_argument('--renorm', action='store_true', help='Rescale intensity to normalise')
+parsearg.add_argument('--stddev', type=int, default=5, help='Number of std devs to highlight')
 
 SPC_DOC_ROOT = "spcctrl"
 
@@ -22,6 +34,7 @@ res = vars(parsearg.parse_args())
 rf = res['rangefile']
 sf = res['specfile']
 renorm = res['renorm']
+stdevs = res['stddev']
 
 if rf is None:
     print "No range file specified"
@@ -65,17 +78,57 @@ print "load complete"
 
 totred = 0.0
 totblue = 0.0
-n = 0
+totboth = 0.0
+resultdict = dict()
+
 for dataset in spclist.datalist:
     try:
         xvalues = dataset.get_xvalues(False)
         yvalues = dataset.get_yvalues(False)
     except specdatactrl.SpecDataError:
         continue
-    n += 1
-    totred += meanval.mean_value(contr, xvalues, yvalues)
-    totblue += meanval.mean_value(contb, xvalues, yvalues)
+    rr, ir = meanval.mean_value(contr, xvalues, yvalues)
+    br, ib = meanval.mean_value(contb, xvalues, yvalues)
+    mvr = ir / rr
+    mvb = ib / br
+    totmv = (ir + ib) / (rr + br)
+    res = intresult(dataset)
+    res.bluecont = mvb
+    res.redcont = mvr
+    res.totcont = totmv
+    resultdict[dataset.modjdate] = res
+    totred += mvr
+    totblue += mvb
+    totboth += totmv
 
-print "Av red=",totred/float(n),"Av blue=",totblue/float(n),"overall=",(totred+totblue)/float(n*2)
+dates = resultdict.keys()
+dates.sort()
+
+redconts = [resultdict[r].redcont for r in dates]
+blueconts = [resultdict[r].bluecont for r in dates]
+
+redmean = np.mean(redconts)
+redstddev = np.std(redconts)
+bluemean = np.mean(blueconts)
+bluestddev = np.std(blueconts)
+
+rednote = stdevs * redstddev
+bluenote = stdevs * bluestddev
+
+for rk in dates:
+    datum = resultdict[rk]
+    dat = datum.dataarray.modjdate
+    rd = datum.redcont
+    bl = datum.bluecont
+#    print "%15.6f %15.6f %15.6f" % (dat, rd, bl),
+#    if abs(rd-redmean) >= rednote: print " Except red",
+#    if abs(bl-bluemean) >= bluenote: print " Except blue",
+#    print
+
+num = float(len(dates))
+print "Av red=",totred/num,"Av blue=",totblue/num,"overall=",totboth / num
+plt.plot(dates,redconts,"r",dates,blueconts,"b")
+plt.show()
+
 
 
