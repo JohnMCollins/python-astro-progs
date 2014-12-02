d3a7d8e87049e993f7ea20345ac878f51111839e
John Collins <j.m.collins@herts.ac.uk>
2014-12-02 22:44:40 +0000

    Revise ewplot

--- Numpy/specinteg/ewplot.py
+++ Numpy/specinteg/ewplot.py
@@ -13,18 +13,44 @@ import matplotlib.pyplot as plt
 import exclusions
 import jdate
 
+def parserange(arg):
+    """Parse a range argument, either a single floating point number, in which case assume other end is zero,
+    or a pair. If only a single number add zero. Return a sorted result"""
+    
+    if arg is None: return None
+    
+    bits = string.split(arg, ',')
+    try:
+        if len(bits) == 1:
+            ret = [0, float(arg)]
+        elif len(bits) == 2:
+            ret = map(lambda x: float(x), bits)
+        else:
+            raise ValueError
+    except:
+        print "Did not understand range value of", arg
+        return None
+    
+    ret.sort()
+    return ret
+
 parsearg = argparse.ArgumentParser(description='Plot equivalent width results')
-parsearg.add_argument('--integ', type=str, help='Input integration file (time/intensity)')
+parsearg.add_argument('integ', type=str, nargs=1, help='Input integration file (time/intensity)')
 parsearg.add_argument('--sepdays', type=int, default=10000, help='Separate plots if this number of days apart')
 parsearg.add_argument('--bins', type=int, default=20, help='Histogram bins')
 parsearg.add_argument('--clip', type=float, default=0.0, help='Number of S.D.s to clip from histogram')
 parsearg.add_argument('--gauss', action='store_true', help='Normalise and overlay gaussian on histogram')
 parsearg.add_argument('--sdplot', action='store_true', help='Put separate days in separate figure')
-parsearg.add_argument('--histy', type=str, default='Occurrences', help='Label for histogram Y axis')
-parsearg.add_argument('--histx', type=str, default='Equivalent width (Angstroms)', help='Label for histogram X axis')
-parsearg.add_argument('--ploty', type=str, default='Equivalent width (Angstroms)', help='Label for plot Y axis')
-parsearg.add_argument('--plotx', type=str, default='Days offset from start', help='Label for plot X axis')
+parsearg.add_argument('--yhist', type=str, default='Occurrences', help='Label for histogram Y axis')
+parsearg.add_argument('--xhist', type=str, default='Equivalent width (Angstroms)', help='Label for histogram X axis')
+parsearg.add_argument('--yplot', type=str, default='Equivalent width (Angstroms)', help='Label for plot Y axis')
+parsearg.add_argument('--xplot', type=str, default='Days offset from start', help='Label for plot X axis')
 parsearg.add_argument('--yaxr', action='store_true', help='Put Y axis label on right')
+parsearg.add_argument('--yrange', type=str, help='Range for Y axis')
+parsearg.add_argument('--histyrange', type=str, help='Range for Hist Y axis')
+parsearg.add_argument('--xaxt', action='store_true', help='Put X axis label on top')
+parsearg.add_argument('--xrange', type=str, help='Range for X axis')
+parsearg.add_argument('--histxrange', type=str, help='Range for Hist X axis')
 parsearg.add_argument('--width', type=float, default=4, help='Display width')
 parsearg.add_argument('--height', type=float, default=3, help='Display height')
 parsearg.add_argument('--outprefix', type=str, help='Output file prefix')
@@ -33,9 +59,10 @@ parsearg.add_argument('--excludes', type=str, help='File with excluded obs times
 parsearg.add_argument('--exclcolours', type=str, default='red,green,blue,yellow,magenta,cyan,black', help='Colours for successive exclude reasons')
 parsearg.add_argument('--legpos', type=str, default='best', help='Legend position')
 parsearg.add_argument('--legnum', type=int, default=5, help='Number for legend')
+parsearg.add_argument('--fork', action='store_true', help='Fork off daemon process to show plot and exit')
 
 res = vars(parsearg.parse_args())
-rf = res['integ']
+rf = res['integ'][0]
 sepdays = res['sepdays']
 sdp = res['sdplot']
 outf = res['outprefix']
@@ -44,6 +71,13 @@ clip = res['clip']
 gauss = res['gauss']
 bins = res['bins']
 ytr = res['yaxr']
+xtt = res['xaxt']
+yrange = parserange(res['yrange'])
+xrange = parserange(res['xrange'])
+histyrange = parserange(res['histyrange'])
+histxrange = parserange(res['histxrange'])
+forkoff = res['fork']                   
+
 dims = (res['width'], res['height'])
 
 if rf is None:
@@ -96,17 +130,32 @@ if clip != 0.0:
         maxv = np.max(hvals)
         lx = np.linspace(minv,maxv,250)
         garr = ss.norm.pdf(lx, mv, std)
+        if histyrange is not None:
+            plt.ylim(*histyrange)
+        if histxrange is not None:
+            plt.xlim(*histxrange)
         plt.hist(hvals, bins=bins, normed = True)
         plt.plot(lx, garr)
     else:
+        if histyrange is not None:
+            plt.ylim(*histyrange)
+        if histxrange is not None:
+            plt.xlim(*histxrange)
         plt.hist(hvals,bins=bins)
 else:
+    if histyrange is not None:
+        plt.ylim(*histyrange)
+    if histxrange is not None:
+        plt.xlim(*histxrange)
     plt.hist(vals,bins=bins)
 if ytr:
     plt.gca().yaxis.tick_right()
     plt.gca().yaxis.set_label_position("right")
-histxlab = res['histx']
-histylab = res['histy']
+if xtt:
+    plt.gca().xaxis.tick_top()
+    plt.gca().xaxis.set_label_position("top")
+histxlab = res['xhist']
+histylab = res['yhist']
 if histxlab == "none":
 	histxlab = ""
 if histylab == "none":
@@ -148,8 +197,8 @@ if len(rxvalues) != 0:
 plotcols = string.split(res['plotcolours'], ',')
 colours = plotcols * ((len(rxarray) + len(plotcols) - 1) / len(plotcols))
 
-xlab = res['plotx']
-ylab = res['ploty']
+xlab = res['xplot']
+ylab = res['yplot']
 if xlab == "none":
 	xlab = ""
 if ylab == "none":
@@ -163,6 +212,10 @@ if sdp:
         xa = np.array(xarr) - offs
         ya = np.array(yarr)
         f = plt.figure(figsize=dims)
+        if xrange is not None:
+            plt.xlim(*xrange)
+        if yrange is not None:
+            plt.ylim(*yrange)
         if len(ylab) == 0:
             plt.yticks([])
         else:
@@ -173,6 +226,9 @@ if sdp:
         if len(xlab) == 0:
             plt.xticks([])
         else:
+            if xtt:
+                plt.gca().xaxis.tick_top()
+                plt.gca().xaxis.set_label_position('top')
             plt.xlabel(xlab)
         plt.plot(xa,ya,col,label=jdate.display(xarr[0]))
         if excludes is not None:
@@ -196,6 +252,8 @@ if sdp:
 else:
     legends = []
     lines = []
+    if yrange is not None:
+        plt.ylim(*yrange)
     if len(ylab) == 0:
         plt.yticks([])
     else:
@@ -206,6 +264,9 @@ else:
     if len(xlab) == 0:
         plt.xticks([])
     else:
+        if xtt:
+            plt.gca().xaxis.tick_top()
+            plt.gca().xaxis.set_label_position('top')
         plt.xlabel(xlab)
     for xarr, yarr, col in zip(rxarray,ryarray,colours):
         offs = xarr[0]
@@ -230,7 +291,16 @@ else:
     if outf is not None:
         fname = outf + "_f.png"
         plt.savefig(fname)
-try:
-    plt.show()
-except KeyboardInterrupt:
-    pass
+        sys.exit(0)
+
+# Only display pic if we're not saving
+
+if forkoff:
+    if os.fork() == 0:
+        plt.show()
+else:
+    try:
+        plt.show()
+    except KeyboardInterrupt:
+        pass
+sys.exit(0)
