86b22bac72aa4d63e2ecb75fbabecc6a19db8878
John Collins <j.m.collins@herts.ac.uk>
2015-02-13 15:38:30 +0000

    Work to end Friday

--- Numpy/fsconv/ewandhr.py
+++ Numpy/fsconv/ewandhr.py
@@ -19,6 +19,7 @@ parsearg.add_argument('--ycolumn', help='Column in data for Y values', type=int,
 parsearg.add_argument('--central', type=float, default=6563.0, help='Central wavelength value def=6563')
 parsearg.add_argument('--ithresh', type=float, default=10.0, help='Percent threshold for EW selection')
 parsearg.add_argument('--continuum', type=float, default=1.0, help='Continuum value')
+parsearg.add_argument('--degfit', type=int, default=10, help='Degree of fitting polynomial')
 parsearg.add_argument('--outfile', type=str, help='Output file')
 
 resargs = vars(parsearg.parse_args())
@@ -37,7 +38,8 @@ xcolumn = resargs['xcolumn']
 ycolumn = resargs['ycolumn']
 
 central = resargs['central']
-ithreshold = resargs['ithresh']
+degfit = resargs['degfit']
+ithreshold = resargs['ithresh'] / 100.0
 con = resargs['continuum']
 threshv = con + ithreshold / 100.0
 
@@ -51,14 +53,14 @@ obstimes = dict()
 obstimefile = resargs['obstimes']    
 if obstimefile is None:
     print "No obs times file given"
-    sys.exit(8)
+    sys.exit(208)
 obstimes = fakeobs.getfakeobs(obstimefile)
 if obstimes is None:
     print "Cannot read fake obs file", obstimefile
-    sys.exit(9)
+    sys.exit(209)
 if xcolumn == ycolumn:
     print "Cannot have X and Y columns the same"
-    sys.exit(10)
+    sys.exit(210)
 
 results = []
 
@@ -69,15 +71,41 @@ for sf in spec:
         amps = arr[ycolumn]
     except IOError as e:
         print "Could not load spectrum file", sf, "error was", e.args[1]
-        sys.exit(11)
+        sys.exit(211)
     except ValueError:
         print "Conversion error on", sf
-        sys.exit(12)
+        sys.exit(212)
     except IndexError:
         print "Do not believe columns x column", xcolumn, "y column", ycolumn
-        sys.exit(13)
-        
+        sys.exit(213)
+            
     obst = obstimes[sf]
+    
+    # Try to find the main maxima by fitting a polynomial to it thinking that the main peaks
+    # will be the bits we want
+    
+    scaledwl = wavelengths - central
+    coeffs = np.polyfit(scaledwl, amps, degfit)
+    
+    # Get maxima and minima of that
+    
+    dcoeffs = np.polyder(coeffs)
+    ddcoeffs = np.polyder(dcoeffs)
+    maxmin = np.roots(dcoeffs)
+    
+    # only the real ones
+    maxmin = np.real(maxmin[np.imag(maxmin) == 0.0])
+    
+    # only the ones within the given range of wavelengths
+    
+    maxmin = maxmin[(maxmin > np.min(scaledwl)) & (maxmin < np.max(scaledwl))]
+    maxmin = np.sort(maxmin)
+    
+    maxima_values = maxmin[np.polyval(ddcoeffs, maxmin) > 0]
+    minima_values = maxmin[np.polyval(ddcoeffs, maxmin) < 0]
+    
+    print maxima_values, minima_values
+    continue
 
     maxima = ss.argrelmax(amps)[0]
     minima = ss.argrelmin(amps)[0]
@@ -88,6 +116,7 @@ for sf in spec:
 
         ew = hs = 0.0
         hr = 1.0
+        print "Nothing, maxima len =", len(maxima), "minima len =",len(minima)
 
     elif len(maxima) == 1:
 
@@ -100,11 +129,12 @@ for sf in spec:
         ewpl = np.where(sel)[0]
         if len(ewpl) < 2:
             ew = 0.0
+            print "No ewpl in", sf
         else:
             ewplf = ewpl[0]
             ewpll = ewpl[-1]
             ew =  np.trapz(amps[ewplf:ewpll+1]-1.0, wavelengths[ewplf:ewpll+1]) / (wavelengths[ewpll] - wavelengths[ewplf])
-
+            print "Ew 1 peak =", ew
     else:
 
         # Two maxmima with min in between.
@@ -120,6 +150,7 @@ for sf in spec:
         if len(ewpl) < 2:
             ew = hs = 0.0
             hr = 1.0
+            print "No ewplaces in", sf
         else:
             ewplf = ewpl[0]
             ewpll = ewpl[-1]
@@ -127,6 +158,7 @@ for sf in spec:
             ewhi = wavelengths[ewpll]
             ewsz = np.trapz(amps[ewplf:ewpll+1]-1.0, wavelengths[ewplf:ewpll+1])
             ew =  ewsz / (ewhi - ewlow)
+            print "ew set to", ew, "in", sf
 
             # Extract the left and right horns
 
