dcbb55a7a0361ae7174d8a24d0ba6333b3d44c6a
John M Collins <jmc@toad.me.uk>
2015-04-06 23:10:58 +0100

    Make it just generate table.

--- Numpy/periods/pergen.py
+++ Numpy/periods/pergen.py
@@ -9,19 +9,19 @@ import sys
 import numpy as np
 import numpy.random as nr
 import scipy.signal as ss
-import matplotlib.pylab as plt
 
 parsearg = argparse.ArgumentParser(description='Generate periodic data')
-parsearg.add_argument('pars', type=int, nargs='+', help='List of periods and amplitudes')
+parsearg.add_argument('pars', type=float, nargs='+', help='List of periods and amplitudes')
+parsearg.add_argument('--out', type=str, help='Output file')
+parsearg.add_argument('--xmax', type=float, default=100, help='Maximum value for x')
 parsearg.add_argument('--xnum', type=int, default=500, help='Number of x/y values')
-parsearg.add_argument('--sampling', type=int, default=500000, help='Sampling interval')
-parsearg.add_argument('--scaling', type=float, default=1.0, help='Scaling of X values')
+parsearg.add_argument('--extracols', action='store_true', help='Generate output in 4 column format')
 
 resargs = vars(parsearg.parse_args())
 
 xnum = resargs['xnum']
-sampling = resargs['sampling']
-scaling = resargs['scaling']
+xmax = resargs['xmax']
+
 persamps = resargs['pars']
 
 if len(persamps) % 2 != 0:
@@ -31,30 +31,23 @@ if len(persamps) % 2 != 0:
 persamps = np.array(persamps)
 pers = persamps[range(0, len(persamps), 2)]
 amps = persamps[range(1, len(persamps), 2)]
-
 freqs = 2 * np.pi / pers
 
-minfreq = np.min(freqs)
-maxfreq = np.max(freqs)
-
-xv = np.linspace(minfreq * 0.1, maxfreq * 2.0, xnum)
+xv = np.linspace(0, xmax, xnum)
 yv = np.zeros(xnum)
-for f,a in zip(list(freqs),list(amps)):
+
+for f, a in zip(list(freqs), list(amps)):
     yv += a * np.sin(xv * f)
 
-samps = np.linspace(minfreq * 0.9, maxfreq * 1.1, sampling)
-pgm = ss.lombscargle(xv, yv, samps)
-amppgm = np.sqrt(4 * pgm / len(xv))
-perscalc = 2*np.pi/samps
+if resargs['extracols']:
+    z = np.zeros(len(xv))
+    o = z + 1
+    outarray = np.array([xv, yv, z, o])
+else:
+    outarray = np.array([xv, yv])
+outarray = np.transpose(outarray)
 
-mxamp = np.max(amppgm)
-maxes = ss.argrelmax(amppgm)[0]
-if len(maxes) > len(pers):
-    imxes = np.argsort(amppgm[maxes])
-    maxes = maxes[imxes[-len(pers):]]
-
-plt.plot(perscalc, amppgm)
-for mx in maxes:
-    plt.axvline(perscalc[mx], color='red')
-    plt.text(perscalc[mx], mxamp * .9, "%.2f" % perscalc[mx], color='red', rotation=90) 
-plt.show()
+if resargs['out'] is None:
+    np.savetxt(sys.stdout, outarray)
+else:
+    np.savetxt(resargs['out'], outarray)
