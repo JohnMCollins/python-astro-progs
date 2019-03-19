90ca5cffff9fd8c0e9b005257f202cfb6e413a0c
John M Collins <jmc@toad.me.uk>
2019-03-19 19:53:39 +0000

    Add iforb fetch.

---
+++ Numpy/remfits/getiforb.pl
@@ -0,0 +1,45 @@
+#! /usr/bin/perl
+
+# @Author: John M Collins <jmc>
+# @Date:   2018-03-24T14:39:41+00:00
+# @Email:  jmc@toad.me.uk
+# @Filename: getfits.pl
+# @Last modified by:   jmc
+# @Last modified time: 2019-01-18T20:38:19+00:00
+
+use dbops;
+use Getopt::Long;
+
+my $base = "remfits";
+
+GetOptions("database=s" => \$base) or die "Invalid options expecting --database dbase";
+
+my $dbase = dbops::opendb($base) or die "Cannot open DB $base";
+
+$sfh = $dbase->prepare("SELECT iforbind,ffname FROM iforbinf WHERE ind=0 ORDER by date_obs");
+$sfh->execute;
+
+$root_url = 'http://ross.iasfbo.inaf.it';
+
+while (my $row = $sfh->fetchrow_arrayref())  {
+    my ($iforbind, $ffname) = @$row;
+    my $fitsfile = "";
+    my $next;
+    open(WG, "wget -q -O - $root_url/REMDBdev/fits_retrieve.php?ffile=/Ross/$ffname|") or die "Cannot create pipe";
+    while (sysread(WG,$next,4096) > 0)  {
+	   $fitsfile .=	$next;
+    }
+    close WG;
+    if (length($fitsfile) < 10000 || (substr $fitsfile, 0, 6) eq '<html>')  {
+        print "Could not fetch $ffname\n";
+        next;
+    }
+    my $iq = $dbase->prepare("INSERT INTO fitsfile (fitsgz) VALUES (?)");
+    $iq->execute($fitsfile);
+    $iq = $dbase->prepare("SELECT LAST_INSERT_ID()");
+    $iq->execute;
+    my $rr = $iq->fetchrow_arrayref();
+    my $ind = $rr->[0];
+    $iq = $dbase->prepare("UPDATE iforbinf SET ind=$ind WHERE iforbind=$iforbind");
+    $iq->execute;
+}
