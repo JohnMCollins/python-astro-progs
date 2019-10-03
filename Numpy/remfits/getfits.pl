b0effd7cd1c2307cd652da44fb6c3ab8a41abe94
John M Collins <jmc@toad.me.uk>
2019-10-03 09:11:40 +0100

    Add progress

--- Numpy/remfits/getfits.pl
+++ Numpy/remfits/getfits.pl
@@ -10,11 +10,13 @@
 use dbops;
 use Getopt::Long;
 use remdefaults;
+use progress;
 
 my $base = remdefaults::default_database;
 my $plusremir = 0;
+my $verbose = 0;
 
-GetOptions("database=s" => \$base, "remir" => \$plusremir) or die "Invalid options expecting --database dbase [--remir]";
+GetOptions("database=s" => \$base, "remir" => \$plusremir, "verbose" => \$verbose) or die "Invalid options expecting --database dbase [--remir] [--verbose]";
 
 my $dbase = dbops::opendb($base) or die "Cannot open DB $base";
 
@@ -25,12 +27,24 @@ else {
 	$plusremir = " dithID=0 AND";  
 }
 
+if ($verbose)  {
+	$sfh = $dbase->prepare("SELECT COUNT(*) FROM obsinf WHERE$plusremir ind=0 AND rejreason IS NULL ORDER by date_obs");
+    $sfh->execute;
+    my $row = $sfh->fetchrow_arrayref();
+    my ($limit) = @$row;
+	$prog = progress->new(-limit => $limit);
+}
+
 $sfh = $dbase->prepare("SELECT ffname,dithID,obsind FROM obsinf WHERE$plusremir ind=0 AND rejreason IS NULL ORDER by date_obs");
 $sfh->execute;
 
 $root_url = 'http://ross.iasfbo.inaf.it';
 
+my $count = 0;
+my $errors = 0;
+
 while (my $row = $sfh->fetchrow_arrayref())  {
+	$count++;
     my ($ffname, $dithid, $obsind) = @$row;
     if ($dithid == 0)  {
         $ffname = 'Ross/' . $ffname;
@@ -48,7 +62,8 @@ while (my $row = $sfh->fetchrow_arrayref())  {
     if (length($fitsfile) < 10000 || (substr $fitsfile, 0, 6) eq '<html>')  {
     	my $sfh = $dbase->prepare("UPDATE obsinf SET rejreason='FITS file not found' WHERE obsind=$obsind");
     	$sfh->execute;
-        print "Could not fetch $ffname\n";
+        print STDERR  "Could not fetch $ffname\n";
+        $errors++;
         next;
     }
     my $iq = $dbase->prepare("INSERT INTO fitsfile (fitsgz) VALUES (?)");
@@ -59,4 +74,10 @@ while (my $row = $sfh->fetchrow_arrayref())  {
     my $ind = $rr->[0];
     $iq = $dbase->prepare("UPDATE obsinf SET ind=$ind WHERE obsind=$obsind");
     $iq->execute;
+    $prog->update($count) if $verbose;
 }
+$prog->finish if $verbose;
+print STDERR "$count FITS files loaded";
+print STDERR " 1 error" if $errors == 1;
+print STDERR " $errors errors" if $errors > 1;
+print STDERR "\n";
--- Numpy/remfits/getiforb.pl
+++ Numpy/remfits/getiforb.pl
@@ -10,19 +10,33 @@
 use dbops;
 use Getopt::Long;
 use remdefaults;
+use progress;
 
 my $base = remdefaults::default_database;
+my $verbose = 0;
 
-GetOptions("database=s" => \$base) or die "Invalid options expecting --database dbase";
+GetOptions("database=s" => \$base, "verbose" => \$verbose) or die "Invalid options expecting --database dbase";
 
 my $dbase = dbops::opendb($base) or die "Cannot open DB $base";
+
+if ($verbose)  {
+    $sfh = $dbase->prepare("SELECT COUNT(*) FROM iforbinf WHERE ind=0 AND rejreason IS NULL");
+    $sfh->execute;
+    my $row = $sfh->fetchrow_arrayref();
+    my ($limit) = @$row;
+    $prog = progress->new(-limit => $limit);
+}
 
 $sfh = $dbase->prepare("SELECT iforbind,ffname FROM iforbinf WHERE ind=0 AND rejreason IS NULL ORDER by date_obs");
 $sfh->execute;
 
 $root_url = 'http://ross.iasfbo.inaf.it';
 
+my $count = 0;
+my $errors = 0;
+
 while (my $row = $sfh->fetchrow_arrayref())  {
+	$count++;
     my ($iforbind, $ffname) = @$row;
     my $fitsfile = "";
     my $next;
@@ -34,7 +48,8 @@ while (my $row = $sfh->fetchrow_arrayref())  {
     if (length($fitsfile) < 10000 || (substr $fitsfile, 0, 6) eq '<html>')  {
     	my $sfh = $dbase->prepare("UPDATE iforbinf SET rejreason='FITS file not found' WHERE iforbind=$iforbind");
         $sfh->execute;
-        print "Could not fetch $ffname\n";
+        print STDERR "Could not fetch $ffname\n";
+        $errors++;
         next;
     }
     my $iq = $dbase->prepare("INSERT INTO fitsfile (fitsgz) VALUES (?)");
@@ -45,4 +60,11 @@ while (my $row = $sfh->fetchrow_arrayref())  {
     my $ind = $rr->[0];
     $iq = $dbase->prepare("UPDATE iforbinf SET ind=$ind WHERE iforbind=$iforbind");
     $iq->execute;
+    $prog->update($count) if $verbose;
 }
+$prog->finish if $verbose;
+print STDERR "$count daily flat/bias FITS files loaded";
+print STDERR " 1 error" if $errors == 1;
+print STDERR " $errors errors" if $errors > 1;
+print STDERR "\n";
+
