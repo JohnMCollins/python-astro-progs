5d902b919ab79735566807bf730bfc2d02e202d8
John M Collins <jmc@toad.me.uk>
2020-06-30 12:31:24 +0100

    Add time tracking.

--- Numpy/remfits/adddims_to_fits.py
+++ Numpy/remfits/adddims_to_fits.py
@@ -10,6 +10,7 @@ import sys
 import remdefaults
 import io
 import gzip
+import datetime
 
 # Shut up warning messages
 
@@ -33,6 +34,8 @@ if nfits == 0:
     sys.exit(1)
     
 ndone = 0
+nalready = 0
+starttime = datetime.datetime.now()
 
 for dbrow in dbrows:
     ind, rows, cols, startx, starty = dbrow
@@ -43,18 +46,40 @@ for dbrow in dbrows:
     hdr = ff[0].header
     dat = ff[0].data
     ff.close()
-    hdr['startX'] = (startx, 'Starting CCD pixel column')
-    hdr['endX'] = (startx + cols, 'Ending CCD pixel column+1')
-    hdr['startY'] = (starty, 'Starting CCD pixel row')
-    hdr['endY'] = (starty + rows, 'Ending CCD pixel row+1')
-    hdu = fits.PrimaryHDU(dat, hdr)
-    mm = io.BytesIO()
-    hdu.writeto(mm)
-    fitsgz = gzip.compress(mm.getvalue())
-    dbcurs.execute("UPDATE fitsfile SET fitsgz=%s WHERE ind=" + str(ind), (fitsgz,))
+    if 'startX' in hdr:
+        nalready += 1
+    else:
+        hdr['startX'] = (startx, 'Starting CCD pixel column')
+        hdr['endX'] = (startx + cols, 'Ending CCD pixel column+1')
+        hdr['startY'] = (starty, 'Starting CCD pixel row')
+        hdr['endY'] = (starty + rows, 'Ending CCD pixel row+1')
+        hdu = fits.PrimaryHDU(dat, hdr)
+        mm = io.BytesIO()
+        hdu.writeto(mm)
+        fitsgz = gzip.compress(mm.getvalue())
+        dbcurs.execute("UPDATE fitsfile SET fitsgz=%s WHERE ind=" + str(ind), (fitsgz,))
     ndone += 1
     if ndone % 100 == 0:
-        print("Reached %d of %d: %.2f%%" % (ndone, nfits, ndone / nfits * 100.0), file=sys.stderr)
+        todo = ndone / nfits
+        print("Reached %d of %d: %.2f%%" % (ndone, nfits, todo * 100.0), end=' ', file=sys.stderr)
+        ctime = datetime.datetime.now()
+        tdiff = ctime - starttime
+        rate = ndone / tdiff.total_seconds()
+        print("Rate %.2f per sec elapsed" % rate, end=' ', file=sys.stderr)
+        daysd = tdiff.days
+        if daysd != 0:
+            print("%d days" % daysd, end='', file=sys.stderr)
+            tdiff -= datetime.timedelta(days=daysd)
+        hoursd = tdiff.seconds // 3600
+        tdiff -= datetime.timedelta(hours=hoursd)
+        minsd = tdiff.seconds // 60
+        tdiff -= datetime.timedelta(minutes=minsd)
+        secsd = tdiff.total_seconds()
+        print("%dh %dm %.2fs" % (hoursd, minsd, secsd), end='', file=sys.stderr)
+        ediff = datetime.timedelta(seconds=todo / rate)
+        eta = ctime + ediff
+        print(eta.strftime("ETA %d/%m/%Y %H:%M:%S"), file=sys.stderr)
         mydb.commit()
 
 mydb.commit()
+print("Run complete", nalready, "were done already", file=sys.stderr)
